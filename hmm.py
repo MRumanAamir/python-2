f636ef1fa688511addd9528077a5adc3079dc6b7
rumanaamir <rumanaamir2022@gmail.com>
2024-08-25 10:44:52 +0000

    hmmmm

---
+++ hmm.py
@@ -0,0 +1,32 @@
+# userName : str = "ruman";
+# print (userName);
+
+# userName2 : str = 'ruman';
+# print (userName2);
+
+# userName3 : str = "'ruman'";
+# print (userName3);
+
+# userName4 : str = '"ruman"';
+# print (userName4);
+
+# userName5 : str = """ruman""";
+# print (userName5);
+
+# name : str = "ruman";
+# lastName : str = "aamir";
+# print ( name + lastName )
+
+# name : str = "ruman";
+# lastName : str = "aamir";
+# software : str = "software engineer"; 
+# sentence : str = f"{name} {lastName} is a student and a {software}";
+# print (sentence);
+
+# name : str = "ruman";
+# lastName : str = "aamir";
+# software : str = "software engineer"; 
+# sentence : str = "{} {} is a student and a {}" .format(name , lastName , software);
+# print (sentence);
+
+
